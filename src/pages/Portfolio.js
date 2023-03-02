8332a0e6cba75cbe01c195b113f455c35db8820e
matiasbellaud <bellaud.matias@gmail.com>
2023-03-02 21:25:20 +0100

    add the side bar of the web site

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -3,10 +3,12 @@ import About from "../components/About.js";
 import Presentation from "../components/Presentation.js";
 import Works from "../components/Works.js";
 import Header from "../components/Header.js";
+import SideBar from "../components/SideBar.js";
 
 const Portfolio = () => {
   return (
     <div>
+      <SideBar />
       <Header />
       <body>
         <Presentation />
